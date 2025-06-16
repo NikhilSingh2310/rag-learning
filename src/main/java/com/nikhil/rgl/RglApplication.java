8d660a943bfd84f59e772927bc8f40c5b6934e0f
Nikhil Singh <thakurnikhilsingh2004@gmail.com>
2025-06-17 04:06:44 +0530

    Initial Commit

---
+++ src/main/java/com/nikhil/rgl/RglApplication.java
@@ -0,0 +1,13 @@
+package com.nikhil.rgl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RglApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RglApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nikhil/rgl/RglApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhil.rgl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RglApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
